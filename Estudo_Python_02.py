fd3d48b7031e05295c3a9ba5cba0c273e47cecd3
Sidnei Lima Santos <sidneicasa123@outlook.com>
2022-06-14 20:48:47 -0300

    Este arquivo é um exemplo de estrutura de WHILE no Python.

--- Estudo_Python_02.py
+++ Estudo_Python_02.py
@@ -1,6 +1,16 @@
-qtde = 0
+qtde = 1
+qtde2 = 1
 
 while qtde <= 10:
-    print(qtde)
+
+    while qtde2 <= 10:
+
+        print(qtde, "X", qtde2, "=", qtde*qtde2)
+
+        qtde2 = qtde2 + 1
+
     qtde = qtde + 1
-print("Fim do Loop!")
+
+    qtde2 = 1
+
+print("passou")
