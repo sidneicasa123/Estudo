08ec8f1fdfec21d155bda1b72766f781b53809d1
Sidnei Lima Santos <sidneicasa123@outlook.com>
2022-06-15 20:30:56 -0300

    Tudo sobre Lista em Python

---
+++ Python/Estudo_Python_04.py
@@ -0,0 +1,21 @@
+
+vetor = []
+for i in range(11, 0, -1):
+    vetor.append(i)
+
+# print(vetor[:])
+
+vetor
+print(vetor)
+print(vetor.index(10))
+# print(vetor.pop(1))
+# print(vetor)
+#vetor.insert(1, 2)
+vetor.sort()
+print(vetor)
+
+
+lista01 = [1, 1, 1, 1, 1]
+lista02 = [1, 1, 1, 1, 2]
+
+print(lista01 != lista02)
