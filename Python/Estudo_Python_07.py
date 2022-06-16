5f4b125d25d2e646552a5d12df0da683c4fa7530
Sidnei Lima Santos <sidneicasa123@outlook.com>
2022-06-16 11:45:10 -0300

    Tudo sobre Lambda em Python

---
+++ Python/Estudo_Python_07.py
@@ -0,0 +1,26 @@
+
+#Lambda
+
+Imprimir = (lambda : print("Deu certo"))
+
+Imprimir()
+
+
+def ver(item):
+    return item
+
+ver(Imprimir())
+
+
+#Lambida
+
+Imprimir = (lambda ipr: print(ipr))
+
+Imprimir("Sidnei Lima Santos")
+
+
+def ver(item):
+    return item
+
+
+ver(Imprimir("teste"))
